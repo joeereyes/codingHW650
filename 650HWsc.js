f18805adc9968fc8a9d74f7ff8906e9ed78e9f79
joe_reyes <joeereyes@gmail.com>
2018-04-17 20:52:07 -0700

    stuff

---
+++ 650HWsc.js
@@ -0,0 +1,19 @@
+require(["esri/Map", "esri/views/MapView", "dojo/domReady!"],function(Map,MapView)
+     //my code starts here
+     
+    var mapConfig = {
+        basemap: "dark-gray"
+
+    };
+    var myMap = new Map (mapConfig);
+
+    var mapView = new MapView({
+
+                map:myMap,
+                container: "viewDiv",
+                zoom: 4,
+                center: [15, 65] // longitude, latitude
+
+     });
+
+       // my code ends here
